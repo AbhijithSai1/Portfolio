5d5a1cab157d7ef17f748c247071b64a07191f5c
Abhijith Sai <146252712+AbhijithSai1@users.noreply.github.com>
2026-03-03 13:14:52 -0600

    Update NavBar link to updated Resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -116,7 +116,7 @@ export const NavBar = () => {
             </Nav.Link>
 
             <Nav.Link
-              href="https://drive.google.com/file/d/1hi4Snf3uHr_pFOM_PhSaoOfLpruLnOl4/view?usp=sharing"
+              href="https://drive.google.com/file/d/1MSDhND5SZjjPx9vKcNNkVobUX3okcJwH/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="navbar-link"
